3e6ca1b1b38f4b1bb59946281b1c39a08fd788e5
Viooooletta <violeta.karavaj@gmail.com>
2024-09-24 22:18:14 +0300

    ПОЖАЛУЙСТА ЗАКОМИТЬСЯ

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,4 @@
 //BABADZAKIIII
-//BABADZAKIIII2222222
 public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
