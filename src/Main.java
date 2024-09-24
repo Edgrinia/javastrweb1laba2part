44b9699cb8afcc16474b8708b2ffbef7047f4351
Viooooletta <violeta.karavaj@gmail.com>
2024-09-24 22:07:34 +0300

    ПОЖАЛУЙСТА ЗАКОМИТЬСЯ

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,6 @@
 public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
         System.out.printf("Hello and welcome!");
 
         for (int j = 1; j <= 10; j++) {
