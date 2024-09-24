fee591a64811eeb2f713735cf2c712b6f910d5b4
Viooooletta <violeta.karavaj@gmail.com>
2024-09-24 22:05:04 +0300

    ПОЖАЛУЙСТА ЗАКОМИТЬСЯ

--- src/Main.java
+++ src/Main.java
@@ -8,14 +8,12 @@ public class Main {
 
         for (int j = 1; j <= 10; j++) {
             //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
             System.out.println("j = " + j);
         }
 
         for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
             System.out.println("i = " + i);
         }
     }
 }
+// ILoveYou♥♥♥
